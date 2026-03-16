67eca13d6b9b78ff5dcff4f1bff2225b692098d2
madesh6554 <madesh6554@gmail.com>
2026-03-16 12:47:05 +0530

    style: fix scroll indicator alignment on home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -343,7 +343,7 @@ const Home = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 1.5 }}
-        className="absolute bottom-4 sm:bottom-8 left-1/2 transform -translate-x-1/2"
+        className="absolute bottom-4 sm:bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center"
       >
         <motion.button
           onClick={() => scrollToSection('about')}
